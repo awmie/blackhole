852c700214d7feff014f70db4ee0ef36f80a859d
awmie <amritavakole@gmail.com>
2026-02-17 16:32:48 +0530

    fix: add output export and distDir config for GitHub Pages

--- dist/static/chunks/src_app_page_tsx_52a0d812._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_components_event-horizon_76065447._.js",
-  "static/chunks/node_modules_1520b222._.js",
-  "static/chunks/src_50f1d13b._.js"
-],
-    source: "dynamic"
-});
