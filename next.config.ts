d93307711b6fc06ad9b3712b1ef8594d7533e42b
awmie <amritavakole@gmail.com>
2026-02-17 16:56:07 +0530

    fix: add base path for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'export',
   distDir: 'dist',
+  basePath: '/blackhole',
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
